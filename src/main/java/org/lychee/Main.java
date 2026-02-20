900b300a590018f62f0adeadf0007eb85c5a7fe4
Goshikito <tiffanymak567@gmail.com>
2026-02-20 14:52:51 -0600

    changes to the zestparser

--- src/main/java/org/lychee/Main.java
+++ src/main/java/org/lychee/Main.java
@@ -1,10 +1,13 @@
 package org.lychee;
 
 import org.lychee.gui.LycheeFrame;
+import org.lychee.zest.ZestParser;
 
 public class Main {
 	public static void main(String[] args) {
-		LycheeFrame frame = new LycheeFrame();
-		frame.init();
+//		LycheeFrame frame = new LycheeFrame();
+//		frame.init();
+		ZestParser parse = new ZestParser();
+		parse.parseFile("testing.txt");
 	}
 }
--- src/main/java/org/lychee/zest/EraseCommand.java
+++ src/main/java/org/lychee/zest/EraseCommand.java
@@ -3,8 +3,22 @@ package org.lychee.zest;
 import java.awt.Graphics2D;
 
 public class EraseCommand extends Command {
+	private final Coordinate start;
+	private final Coordinate end;
+	private final int width;
+
+	public EraseCommand(Coordinate start, Coordinate end, int width) {
+		this.start = start;
+		this.end = end;
+		this.width = width;
+	}
+
 	@Override
 	public Shape execute(Graphics2D graphics) {
 		return null;
+	}
+
+	public String toString() {
+		return start + " | " + end + " | " + width;
 	}
 }
