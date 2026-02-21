f99e4d53f5b86ea38fb2c421c001d31b12a7fcba
Goshikito <tiffanymak567@gmail.com>
2026-02-20 20:14:17 -0600

    adds colors to commands

--- src/main/java/org/lychee/zest/Command.java
+++ src/main/java/org/lychee/zest/Command.java
@@ -1,7 +1,27 @@
 package org.lychee.zest;
 
-import java.awt.Graphics2D;
+import java.awt.*;
 
 public abstract class Command {
 	abstract public Shape execute(Graphics2D graphics);
+
+	public Color changeColor(boolean hex, String color) {
+		if (hex) {
+			Color newColor = Color.decode(color);
+			return newColor;
+		} else {
+			return switch (color) {
+				case "black" -> Color.BLACK;
+				case "white" -> Color.WHITE;
+				case "red" -> Color.RED;
+				case "orange" -> Color.ORANGE;
+				case "yellow" -> Color.YELLOW;
+				case "green" -> Color.GREEN;
+				case "blue" -> Color.BLUE;
+				case "purple" -> Color.MAGENTA;
+				case "pink" -> Color.PINK;
+				default -> Color.BLACK;
+			};
+		}
+	}
 }
--- src/main/java/org/lychee/zest/LineCommand.java
+++ src/main/java/org/lychee/zest/LineCommand.java
@@ -8,22 +8,35 @@ public class LineCommand extends Command {
 	private final Coordinate start;
 	private final Coordinate end;
 	private final int width;
+	private final String color;
 
 	public LineCommand(Coordinate start, Coordinate end, int width) {
 		this.start = start;
 		this.end = end;
 		this.width = width;
+		color = null;
+	}
+
+	public LineCommand(Coordinate start, Coordinate end, int width, String color) {
+		this.start = start;
+		this.end = end;
+		this.width = width;
+		this.color = color;
 	}
 
 	@Override
 	public Shape execute(Graphics2D graphics) {
-		graphics.setColor(LycheeColors.RED);
+		if (color != null && color.startsWith("#")) {
+			graphics.setColor(changeColor(true, color));
+		} else if (color != null) {
+			graphics.setColor(changeColor(false, color));
+		}
 		graphics.setStroke(new BasicStroke(width));
 		graphics.drawLine(start.getX(), start.getY(), end.getX(), end.getY());
 		return null;
 	}
 
 	public String toString() {
-		return "line: " + start + " | " + end + " | " + width;
+		return "line: " + start + " | " + end + " | " + width + " | " + color;
 	}
 }
