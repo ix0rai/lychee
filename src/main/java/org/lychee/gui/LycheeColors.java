9c12e373e5e44a733f9472afc897323c673632b3
ix0rai <ix0rai64@gmail.com>
2026-02-21 10:56:29 -0600

    delete unused error panel

--- src/main/java/org/lychee/gui/LycheeColors.java
+++ src/main/java/org/lychee/gui/LycheeColors.java
@@ -7,6 +7,5 @@ public class LycheeColors {
 	public static final Color DARK_RED = new Color(0x68161a);
 	public static final Color PINK = new Color(0xebc0c5);
 	public static final Color BROWN = new Color(0x64200a);
-	public static final Color OFF_WHITE = new Color(0xd8d4cb);
 	public static final Color WHITE = new Color(0xffffff);
 }
